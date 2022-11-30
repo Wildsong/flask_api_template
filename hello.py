c7c20dbb89615e700228466af7ed8e5049faeba6
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-11-30 15:29:34 -0800

    make it an api

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    msg = ''
-    for item in os.environ.items():
-        msg += f"{item[0]} : {item[1]} <br/>"
-    return "<h1>Hello World!</h1>" + msg
-
+@api.route("/env")
+class HelloEnvironment(Resource):
+    def get(self):
+        return dict(os.environ)
+    
