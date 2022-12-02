4c78f84437d378a0dc7f13f943d90957bacea0b9
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-12-02 14:38:56 -0800

    more stuff works now

--- user_api.py
+++ user_api.py
@@ -33,66 +33,106 @@ class UserDAO(object):
     def __init__(self) -> None:
         return
     def getAll(self) -> list:
-        users = db.session.execute(db.select(User)).all()
-        l = [{"id":user[0].id, "username":user[0].username} for user in users]
-    def get(self, id: int) -> object:
-        user = db.session.execute(db.select(User, id=id)).one()
-        return {"id":user[0].id, "username":user[0].username}
-    def create(self, username) -> object:
-        user = None
+        users = db.session.execute(db.select(User).order_by(User.username)).all()
+        l = [u[0].__dict__ for u in users] # return rows as a list of dicts 
+        return l
+    def getUser(self, id: int) -> object:
         try:
-            user = User(
-                username=username,
-                password="joe",
-                role="Producer"
-            )
-            db.session.add(user)
+            # https://docs.sqlalchemy.org/en/14/core/selectable.html#sqlalchemy.sql.expression.select
+            u = db.session.execute(db.select(User).where(User.id==id)).one()[0]
+        except Exception as e:
+            api.abort(404, f'User {id} not found.')
+        return u
+    def get(self, id: int) -> dict:
+        u = self.getUser(id)
+        return u.__dict__ # return row as a dict
+    def create(self, data: object) -> object:
+        u = User()
+        try:
+            u = User()
+            u.username = data['username']
+            if 'password' in data: u.password = data['password']
+            if 'role' in data: u.role = data['role']
+            db.session.add(u)
             db.session.commit()
         except Exception as e:
-            raise UserAlreadyExists
-    def update(self, id: int, data: object) -> object:
-        return
+            print(e)
+            api.abort(404, f"User '{data['username']}' create failed.")
+        return u
+    def update(self, id: int, data: dict) -> dict:
+        u = self.getUser(id)
+        try:
+            if 'username' in data: u.username = data['username']
+            if 'password' in data: u.password = data['password']
+            if 'role'     in data: u.role     = data['role']
+        except Exception as e:
+            print(e)
+            api.abort(404, f'User {id} could not be updated.')
+        db.session.commit()
+        return u
     def delete(self, id: int) -> None:
-        return
+        u = self.getUser(id)
+        try:
+            db.session.delete(u)
+            db.session.commit()
+            s = 'deleted'
+        except Exception as e:
+            print(e)
+            api.abort(404, f'User {id} delete failed.')
+        return u.__dict__
 
 dao = UserDAO()
 
 # This defines what the API sees when it marshals a user from the database
 userModel = api.model('User', {
-    'username': fields.String
+    'id': fields.Integer,
+    'username': fields.String,
+    'role': fields.String
 })
 
 def UserAlreadyExists(Error):
     return
-@ns.route('/')
+@ns.route('/') # This will be the URL with a slash at the end, http://127.0.0.1:5000/user/
 class UserList(Resource):
     @ns.doc('list_users')
-
     @ns.marshal_list_with(userModel)
     def get(self):
         """ Return a JSON formatted list of all users in the database. """
+        # I want to return the URL for each user
+        return dao.getAll()
 
+    @ns.doc('create_todo')
+    @ns.expect(userModel)
+    @ns.marshal_with(userModel, code=201)
+    def post(self):
+        """ Add a new user to the database. """
+        # I want to return the URL for the updated user
+        return dao.create(api.payload), 201
+        
 
-@ns.route('/user/<int:id>', methods=["GET"])
-@api.route('/user', methods=["POST"])
+@ns.route('/<int:id>')
+@ns.response(404, 'User not found')
 class UserApi(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username', type=str, required=True,
         help='Login name of a user', location='json')
 
+    @ns.doc('get_user')
+    @ns.marshal_with(userModel)
     def get(self, id):
         """ Return information on a user matching the requested id. """
-        user = db.session.execute(db.select(User).filter_by(id=id)).one()
-        u = {"id":user[0].id, "username":user[0].username}
-        return u
+        # I want to return the URL for the updated user
+        return dao.get(id)
 
-    def post(self):
-        """ Add a new user to the database. """
-        args = self.parser.parse_args()
-        u = args['username'].lower()
-        return dao.create(u)
+    @ns.doc('update_user')
+    @ns.expect(userModel)
+    @ns.marshal_with(userModel, code=201)
+    def put(self, id):
+        """ Update an existing user. """
+        # I want to return the URL for the updated user
+        return dao.update(id, api.payload), 201
         
-    def delete(self):
-        print("DELETE!")
-        return jsonify(count=1)
+    @ns.doc('delete_user')
+    def delete(self, id):
+        return dao.delete(id)
 
